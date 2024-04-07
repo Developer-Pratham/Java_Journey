022012e222b11fd1d7c103b9fa3c296d2030195f
Developer-Pratham <indeedpratham@gmail.com>
2024-04-07 21:31:30 +0530

    Added code for prime number

---
+++ prime.java
@@ -0,0 +1,21 @@
+// This code will find if the given number by the user is prime or not
+
+import java.io.Console;
+public class prime {
+    public static void main(String[] args) {
+        Console c = System.console();
+        int num = Integer.parseInt(c.readLine("Enter the number :"));
+        int temp = 0;
+        for(int i = 1; i<=num; i++){
+            if(num%i==0){
+                temp++;
+            }
+        }
+        if (temp<=2) {
+            System.out.println("The number "+num+" is Prime");
+        }
+        else{
+            System.out.println("The number "+num+" is consonant");
+        }
+    }
+}
