f008b73dbc027ed0d6bad88535fce687c1630878
Developer-Pratham <indeedpratham@gmail.com>
2024-04-08 22:28:12 +0530

    Added all files

--- reverse_string.java
+++ reverse_string.java
@@ -1,10 +1,11 @@
 import java.io.Console;
-class reverse_string{
-public static void main(String args[]){
-Console c = System.console();
-String str = c.readLine("Enter your string here : ");
-for(int i = str.length()-1 ; i>=0 ; i--){
-System.out.print(str.charAt(i));
-}
-}
+
+class reverse_string {
+    public static void main(String args[]) {
+        Console c = System.console();
+        String str = c.readLine("Enter your string here : ");
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+    }
 }
